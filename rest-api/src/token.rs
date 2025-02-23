1ec6a5205063f147ac3fe4bd2bf8bd35f502f940
Nguyen Tuan Anh <v.anhnt605@gmail.com>
2025-02-23 20:05:34 +0700

    init project

---
+++ rest-api/src/token.rs
@@ -0,0 +1,16 @@
+use axum::{http::StatusCode, routing::get, Json, Router};
+
+pub fn router() -> Router {
+    Router::new().route("/health", get(health_check))
+}
+#[derive(serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct HealthyResponse {
+    pub message: String,
+}
+
+async fn health_check() -> Result<Json<HealthyResponse>, (StatusCode, String)> {
+    Ok(Json(HealthyResponse {
+        message: "Healthy".into(),
+    }))
+}
