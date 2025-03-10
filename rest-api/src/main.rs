1fc193c738887e7694e2d93215a02ae885f0de38
Nguyen Tuan Anh <anhnguyensgu@gmail.com>
2025-03-10 10:37:59 +0700

    bind port to webserver

--- rest-api/src/main.rs
+++ rest-api/src/main.rs
@@ -1,6 +1,8 @@
 use app::{print_request_response, AppState};
 use axum::routing::get;
 use axum::{middleware, Router};
+use std::env;
+use tracing::info;
 
 pub mod app;
 pub mod config;
@@ -28,12 +30,15 @@ async fn main() {
 
     let router = Router::new()
         .route("/price/{address}", get(price::route::get_price))
+        .route("/health", get(token::search::search_token))
         .with_state(app_state)
         .layer(middleware::from_fn(print_request_response));
 
     let app = r.nest("/api/v1", router);
+    let port = env::var("PORT").expect("PORT environment variable not set");
+    info!("starting http server on port {port}");
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
         .await
         .expect("should create listener");
 
--- rest-api/src/token/search.rs
+++ rest-api/src/token/search.rs
@@ -3,7 +3,7 @@ use axum::{http::StatusCode, Json};
 use super::HealthyResponse;
 
 #[allow(dead_code)]
-async fn search_token() -> Result<Json<HealthyResponse>, (StatusCode, String)> {
+pub async fn search_token() -> Result<Json<HealthyResponse>, (StatusCode, String)> {
     Ok(Json(HealthyResponse {
         message: "Healthy".into(),
     }))
