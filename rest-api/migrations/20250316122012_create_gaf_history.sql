3a8e2c0838395ffcb8ce59f16c0a231db1f1d9cc
Nguyen Tuan Anh <anhnguyensgu@gmail.com>
2025-03-16 19:21:44 +0700

    commit new schema

---
+++ rest-api/migrations/20250316122012_create_gaf_history.sql
@@ -0,0 +1,11 @@
+-- Add migration script here
+create table if not exists greed_and_fear_history
+(
+    unix_timestamp       bigint not null
+        constraint greed_and_fear_history_pk
+            primary key,
+    recorded_at          timestamp with time zone,
+    value                integer,
+    value_classification varchar(96),
+    chain                varchar(96) default 'solana'::character varying
+);
