91363563a9dd5438e650268ec994e5c25b7abd8b
minhthong1410 <minhthongvt1410@gmail.com>
2025-03-25 13:37:34 +0700

    Revert "bind port to webserver"
    This reverts commit 00a1656942351c5bfcdb20b24aea9b99cffadbdd.

--- rest-api/src/token/search.rs
+++ rest-api/src/token/search.rs
@@ -3,7 +3,7 @@ use axum::{http::StatusCode, Json};
 use super::HealthyResponse;
 
 #[allow(dead_code)]
-pub async fn search_token() -> Result<Json<HealthyResponse>, (StatusCode, String)> {
+async fn search_token() -> Result<Json<HealthyResponse>, (StatusCode, String)> {
     Ok(Json(HealthyResponse {
         message: "Healthy".into(),
     }))
