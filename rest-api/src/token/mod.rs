235799f7c3734c5e1cee3381a5edbd3013421802
pepeminor <thang.nq.666@gmail.com>
2025-03-27 16:54:53 +0700

    Mock api token distributions

--- rest-api/src/token/mod.rs
+++ rest-api/src/token/mod.rs
@@ -3,7 +3,9 @@ pub mod model;
 pub mod health;
 mod trending;
 mod background_job;
+mod distributions;
 
 pub use route::*;
 pub use trending::*;
 pub use background_job::*;
+pub use distributions::*;
