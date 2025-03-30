d009d02dad5c9afc7d77e22e20871bab3dabe707
pepeminor <thang.nq.666@gmail.com>
2025-03-30 20:00:17 +0700

    Update create table

--- rest-api/migrations/20250328170349_create_token_analytics_table.sql
+++ rest-api/migrations/20250328170349_create_token_analytics_table.sql
@@ -1,19 +1,10 @@
--- Add migration script here
 CREATE TABLE token_analytics (
     address TEXT PRIMARY KEY,
-    market_cap DOUBLE PRECISION,
-    market_cap_change_7d DOUBLE PRECISION,
-    market_cap_7d_historical_values JSONB,
-    volume_24h DOUBLE PRECISION,
-    volume_24h_change_7d DOUBLE PRECISION,
-    volume_historical JSONB,
-    liquidity DOUBLE PRECISION,
-    liquidity_change DOUBLE PRECISION,
-    liquidity_historical JSONB,
-    holders BIGINT,
-    holders_change_7d BIGINT,
-    holders_historical JSONB,
-    top_followers JSONB,
-    followers JSONB,
-    mentions JSONB
+    market_cap NUMERIC,
+    volume_24h NUMERIC,
+    volume_24h_change_7d NUMERIC,
+    liquidity NUMERIC,
+    liquidity_change NUMERIC,
+    holders NUMERIC,
+    holders_change_7d NUMERIC,
 );
