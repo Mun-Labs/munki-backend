82e751a7bd432da698727a1ae32ef872b2e914cc
minhthong1410 <126005323+minhthong1410@users.noreply.github.com>
2025-04-04 08:03:29 +0700

    fix error go to details if without search (#34)

--- rest-api/src/token/route.rs
+++ rest-api/src/token/route.rs
@@ -9,7 +9,7 @@ use crate::token::{
 };
 use axum::extract::{Path, Query, State};
 use axum::{http::StatusCode, Json};
-use bigdecimal::{BigDecimal, ToPrimitive};
+use bigdecimal::{BigDecimal, FromPrimitive, ToPrimitive};
 use chrono::Utc;
 use serde::Serialize;
 
@@ -352,18 +352,65 @@ pub async fn search_tokens(
         .await?;
     Ok(tokens)
 }
+#[derive(serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TokenDetailResponse {
+    pub token_address: String,
+    pub name: String,
+    pub symbol: String,
+    pub logo_uri: String,
+    pub website_url: String,
+    pub metadata: serde_json::Value,
+    pub decimals: i32,
+    pub marketcap: BigDecimal,
+    pub history24h_price: BigDecimal,
+    pub price_change24h_percent: BigDecimal,
+    pub holders: i32,
+    pub liquidity: BigDecimal,
+    pub volume_24h: BigDecimal,
+    pub volume_24h_change: BigDecimal,
+    pub mun_score: f64,
+    pub total_supply: BigDecimal,
+    pub current_price: BigDecimal,
+    pub risk_score: f64,
+}
+
+impl From<TokenOverviewResponse> for TokenDetailResponse {
+        fn from(value: TokenOverviewResponse) -> Self {
+            Self {
+                token_address: value.token_address,
+                name: value.name,
+                symbol: value.symbol,
+                logo_uri: value.logo_uri.unwrap_or_default(),
+                website_url: value.website_url.unwrap_or_default(),
+                metadata: value.metadata.unwrap_or_default(),
+                decimals: value.decimals.unwrap(),
+                marketcap: value.marketcap.unwrap(),
+                history24h_price: value.history24h_price.unwrap_or_default(),
+                price_change24h_percent: value.price_change24h_percent.unwrap_or_default(),
+                holders: value.holders.unwrap_or(0),
+                liquidity: value.liquidity.unwrap_or_default(),
+                volume_24h: value.volume_24h.unwrap_or_default(),
+                volume_24h_change: value.volume_24h_change.unwrap_or_default(),
+                mun_score: value.mun_score.unwrap().to_f64().unwrap(),
+                total_supply: value.total_supply.unwrap(),
+                current_price: value.current_price.unwrap(),
+                risk_score: value.risk_score.unwrap().to_f64().unwrap(),
+            }
+        }
+    }
 
 pub async fn get_token_bio(
     State(app): State<AppState>,
     Path(address): Path<String>,
-) -> Result<Json<HttpResponse<TokenOverviewResponse>>, (StatusCode, String)> {
+) -> Result<Json<HttpResponse<TokenDetailResponse>>, (StatusCode, String)> {
     let missing = token_by_address(&app.pool, vec![address.clone()])
         .await
         .map_err(|e| {
             error!("Failed to fetch token by address: {}", e);
             (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
         })?;
-    if !missing.is_empty() {
+    let resp: TokenDetailResponse = if !missing.is_empty() {
         let token = fetch_token_details(&app, &address).await.map_err(|e| {
             error!("Failed to fetch token details: {}", e);
             (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
@@ -374,12 +421,38 @@ pub async fn get_token_bio(
             .map_err(|e| {
                 error!("Failed to insert token: {}", e);
                 (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
+            })
+            .map(|_| {
+                TokenDetailResponse {
+                    token_address: token.address,
+                    name: token.name,
+                    symbol: token.symbol,
+                    logo_uri: token.logo_uri.unwrap().to_string(),
+                    website_url: token.website_url.unwrap_or_default().to_string(),
+                    metadata: serde_json::to_value(token.extensions.unwrap_or_default()).unwrap_or_default(),
+                    decimals: token.decimals.to_i32().unwrap(),
+                    marketcap: BigDecimal::from_f64(token.marketcap.unwrap()).unwrap(),
+                    history24h_price: BigDecimal::from_f64(token.history24h_price.unwrap_or_default()).unwrap(),
+                    price_change24h_percent: BigDecimal::from_f64(token.price_change24h_percent.unwrap_or_default()).unwrap(),
+                    holders: token.holder.unwrap_or_default(),
+                    liquidity: BigDecimal::from_f64(token.liquidity.unwrap_or_default()).unwrap(),
+                    volume_24h: BigDecimal::from_f64(token.volume24h.unwrap_or_default()).unwrap(),
+                    volume_24h_change: BigDecimal::from_f64(token.volume_24h_change.unwrap_or_default()).unwrap(),
+                    mun_score: 0.0,
+                    total_supply: BigDecimal::from_f64(token.total_supply.unwrap_or_default()).unwrap(),
+                    current_price: BigDecimal::from_f64(token.price.unwrap()).unwrap(),
+                    risk_score: 0.0,
+                }
+            })?
+    } else {
+        let token_bio_response = token_bio(&app.pool, &address)
+            .await
+            .map_err(|e| {
+                error!("Failed to fetch token bio: {}", e);
+                (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
             })?;
-    }
-    let resp = token_bio(&app.pool, &address).await.map_err(|e| {
-        error!("Failed to fetch token bio: {}", e);
-        (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
-    })?;
+        TokenDetailResponse::from(token_bio_response)
+    };
 
     Ok(Json(HttpResponse {
         code: 200,
